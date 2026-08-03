99f446eeda2c7d91cc363d2c8946e00823f8a038
abrakantrout <abrakant444@gmail.com>
2026-08-03 18:55:39 +0530

    Edited the code for safer use

--- 0933_Number_of_Recent_Calls.py
+++ 0933_Number_of_Recent_Calls.py
@@ -23,15 +23,15 @@ from collections import deque
 class RecentCounter:
 
     def __init__(self):
-        self.RecentCounter = deque()
+        self.queue = deque()
 
     def ping(self, t: int) -> int:
-        self.RecentCounter.append(t)
+        self.queue.append(t)
 
-        while self.RecentCounter[0] < t - 3000:
-            self.RecentCounter.popleft()
+        while self.queue and self.queue[0] < t - 3000:
+            self.queue.popleft()
 
-        return len(self.RecentCounter)
+        return len(self.queue)
 
 
 # Your RecentCounter object will be instantiated and called as such:
